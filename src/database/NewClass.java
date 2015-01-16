877313453297bcebde477ef5698824629086f65a
ma803216 <marie.hotkova@o2.cz>
2015-01-16 16:10:24 +0100

    new comimit

---
+++ src/database/NewClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package database;
+
+/**
+ *
+ * @author ma803216
+ */
+public class NewClass {
+    public NewClass(int a)
+    {
+        System.out.println("ahoj "+a);
+    }
+}
